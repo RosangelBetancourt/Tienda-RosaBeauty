655bc45d5d57b7b4b61336da2e943f6045d909d1
rossjbm <136488713+RosangelBetancourt@users.noreply.github.com>
2024-02-26 17:23:44 -0400

    Revert "Servidor con Formulario "Opiniones""
    This reverts commit 1a120ce0947c17562d6e3ce3a395b66ba802f815.

--- servidor/routes/opiniones-r.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const opinionesControllers = require('../controllers/opiniones-c')
-
-/* GET home page. */
-router.get('/', opinionesControllers.mostrar);
-router.post('/', opinionesControllers.crear);
-
-module.exports = router;
